9bad47b1d0c03798876ed44a68701e3bc79094fa
Victor <victornunesvln2020@gmail.com>
2021-09-27 23:02:39 -0300

    Some fixes

--- so_long/inc/so_long.h
+++ so_long/inc/so_long.h
@@ -5,68 +5,68 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: vlima-nu <vlima-nu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/16 19:11:27 by vlima-nu          #+#    #+#             */
-/*   Updated: 2021/09/16 19:11:27 by vlima-nu         ###   ########.fr       */
+/*   Created: 2021/09/27 22:46:56 by vlima-nu          #+#    #+#             */
+/*   Updated: 2021/09/27 22:46:56 by vlima-nu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include "so_long_init.h"
-
 /*
 ** includes all structs, macros and other headers
 */
-char	*load_map(char *filename);
+# include "so_long_init.h"
+
 /*
-** Opens argv, checks file extension and descriptor
-** and saves the map or exit the program in case of errors.
+** Opens the file, checks file extension and file descriptor
+** returns its content in a char pointer or exit the program in case of errors.
 */
-void	init_struct(t_game *game);
+char	*load_map(char *filename);
 /*
 ** Initializes stack variables of game structure and its sub structures.
 */
-void	start_game(t_game *game);
+void	init_struct(t_game *game);
 /*
-** Saves map info, such as area, height, player position, etc,
+** Saves map information, such as area, height, player position, etc,
 ** Opens a window and get sprites from ./img/xpm
 */
-void	render(t_game *game);
+void	start_game(t_game *game);
 /*
 ** Renders pixel per pixel each map character with this respectives sprites.
 */
-int		exit_game(char *message, int status, t_game *game);
+void	render(t_game *game);
 /*
-** Writes "Error\n" if status equals 1,
+** Writes "Error" if status equals 1,
 ** writes the message passed, free all allocated memory and exit(status).
 */
-void	draw_img(t_img *img, t_img *sprite, int x, int y);
+int		exit_game(char *message, int status, t_game *game);
 /*
 ** Receives the image to be drawn and the sprite
 ** to draw at ((x * 64),(y * 64)) position of the image.
 */
-size_t	get_color(t_img *img, int x, int y);
+void	draw_img(t_img *img, t_img *sprite, int x, int y);
 /*
 ** Returns the pixel color of (x, y) position of the sprite.
 */
-void	draw_pixel(t_img *img, int x, int y, size_t color);
+size_t	get_color(t_img *img, int x, int y);
 /*
 ** Draws the color in (x, y) position of the image.
 */
-char	*ft_strchr(const char *src, int c);
+void	draw_pixel(t_img *img, int x, int y, size_t color);
 /*
 ** Returns a pointer to the first occurrence of the character c
 ** in the string src or NULL if the character is not found.
 */
-char	*ft_utoa(size_t nbr);
+char	*ft_strchr(const char *src, int c);
 /*
 ** Converts unsigned numbers into a string.
 */
-int		ft_strcmp(char *s1, char *s2);
+char	*ft_utoa(size_t nbr);
 /*
 ** Compares the two strings and returns the difference
 ** between then or zero if they're equal.
 */
+int		ft_strcmp(char *s1, char *s2);
 
 #endif
