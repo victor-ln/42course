a6470a699693b2071b1c5261696ffdd3f0f00973
Victor <victornunesvln2020@gmail.com>
2021-11-09 21:27:01 -0300

    Adding some comments

--- minitalk/inc/client.h
+++ minitalk/inc/client.h
@@ -6,20 +6,46 @@
 /*   By: vlima-nu <vlima-nu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 17:40:09 by vlima-nu          #+#    #+#             */
-/*   Updated: 2021/11/09 19:22:15 by vlima-nu         ###   ########.fr       */
+/*   Updated: 2021/11/09 21:02:42 by vlima-nu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLIENT_H
 # define CLIENT_H
 
-# include "../../inc/minitalk.h"
+# include "minitalk.h"
 
+/*
+	Saves the server pid, sets function pointer to send message length,
+	copies the string to be passed and sets connection and signals.
+*/
 void		start_struct(char **args);
+
+/*
+	Checks if the pid format is valid, if the process exists and
+	if you have permission to send the message. Return pid if no error occurs.
+*/
 pid_t		check_pid(char *ptr);
+
+/*
+	Keeps connection with server till string is sent or some error occurs.
+*/
 void		start_connection(void);
+
+/*
+	Checks if server still handling the message.
+*/
 void		get_signal(int signal);
+
+/*
+	Sends the length of the string in 32 bits, then changes process pointer to
+	send the message.
+*/
 void		send_message_len(void);
+
+/*
+	Sends the message, each bit of each byte then sets connection off.
+*/
 void		send_message(void);
 
 #endif
--- minitalk/inc/server.h
+++ minitalk/inc/server.h
@@ -6,20 +6,37 @@
 /*   By: vlima-nu <vlima-nu@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 18:16:09 by vlima-nu          #+#    #+#             */
-/*   Updated: 2021/11/09 19:51:27 by vlima-nu         ###   ########.fr       */
+/*   Updated: 2021/11/09 21:08:46 by vlima-nu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_H
 # define SERVER_H
 
-# include "../../inc/minitalk.h"
+# include "minitalk.h"
 
 # define DIFF_PID "SERVER: Handling signals from another client now\n"
 
+/*
+	Receives the signal SIGUSR1 or SIGUSR2 and some info about the sender.
+	Checks if the signal is the same received from the last client and handles it.
+*/
 void		handler(int signal, siginfo_t *info, void *null);
+
+/*
+	Calculates the length of the string being received and allocates memory for it.
+*/
 void		get_message_len(int signal, t_server *server);
+
+/*
+	Calculates each bit received until completes a byte, and stores its value in the
+	string to be displayed when the number of bytes is equal to the message length.
+*/
 void		get_message(int signal, t_server *server);
+
+/*
+	Frees allocated memory and set to zero most variables.
+*/
 void		restart_server(t_server *server);
 
 #endif
